e0ba55662187192a1790eae16ab0d0e4c3e2619d
sophia-yilmazlab <215802058+sophia-yilmazlab@users.noreply.github.com>
2025-09-20 07:58:29 +0000

    Deploying to gh-pages from @ sophia-yilmazlab/yilmazlab@17eb4073efcb532e66d0fc6cf23de5fa601391c8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,21 +16,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/yilmazlab/teaching/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/yilmazlab/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/yilmazlab/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
