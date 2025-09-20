c37471c435c51a27b0a4dbb25cc7ddf22f032932
sophia-yilmazlab <yilmaz@sophia.ac.jp>
2025-09-20 00:44:28 +0000

    Deploying to gh-pages from @ sophia-yilmazlab/yilmazlab@9f65efaa30d4076919db824866e1e18b507fca93 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "eyilma41/yilmazlab",
+    "data-repo": "sophia-yilmazlab/yilmazlab",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
