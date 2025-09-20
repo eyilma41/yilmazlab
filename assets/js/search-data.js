3609a810ec3bccb1653350280f58c50b5ddaa370
sophia-yilmazlab <yilmaz@sophia.ac.jp>
2025-09-20 06:17:31 +0000

    Deploying to gh-pages from @ sophia-yilmazlab/yilmazlab@19fdcc0b491a163537db0eedae016af76c40c513 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/yilmazlab/";
     },
-  },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/yilmazlab/cv/";
-          },
-        },{id: "nav-teaching",
+  },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for each course can be found on MOODLE page (course registration required).",
           section: "Navigation",
